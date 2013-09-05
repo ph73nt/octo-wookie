2b894c5f415e27e022834ebb771373c1342fd0e3
Neil Thomson <neil@pukka-j.com>
2013-09-05 14:09:51 +0100

    Add least squares fit method and unit test

---
+++ src/couk/nucmedone/common/gsl/LeastSquaresFit.java
@@ -0,0 +1,118 @@
+/**
+ * @author Neil
+ *
+ * Copyright (C) 2013 Neil
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or (at
+ * your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ */
+package couk.nucmedone.common.gsl;
+
+/**
+ * Inspired by gsl-1.16/fit/fit.c from the GNU Scientific Library project.<br />
+ * <br />
+ * Fit the data (<i>x</i>_<i>i</i>, <i>y</i>_<i>i</i>) to the linear
+ * relationship<br />
+ * <br />
+ * 
+ * <i>Y</i> = <i>c</i>0 + <i>c</i>1 <i>x</i> <br />
+ * <br />
+ * returning,<br />
+ * <br />
+ * <i>c</i>0, <i>c</i>1 -- coefficients cov00, cov01, cov11 --
+ * variance-covariance matrix of <i>c</i>0 and <i>c</i>1, sumsq -- sum of
+ * squares of residuals. <br />
+ * <br />
+ * This fit can be used in the case where the errors for the data are uknown,
+ * but assumed equal for all points. The resulting variance-covariance matrix
+ * estimates the error in the coefficients from the observed variance of the
+ * points around the best fit line.
+ */
+public class LeastSquaresFit {
+
+	private double c0;
+	private double c1;
+	private double cov00;
+	private double cov01;
+	private double cov11;
+	private double sumsq=0;
+
+	public LeastSquaresFit(double[] x, double[] y, int n, int xstride,
+			int ystride) {
+
+		double m_x = 0;
+		double m_y = 0;
+
+		for (int i = 0; i < n; i++) {
+			m_x += (x[i * xstride] - m_x) / (i + 1.0);
+			m_y += (y[i * ystride] - m_y) / (i + 1.0);
+		}
+
+		double m_dx2 = 0;
+		double m_dxdy = 0;
+
+		for (int i = 0; i < n; i++) {
+			double dx = x[i * xstride] - m_x;
+			double dy = y[i * ystride] - m_y;
+
+			m_dx2 += (dx * dx - m_dx2) / (i + 1.0);
+			m_dxdy += (dx * dy - m_dxdy) / (i + 1.0);
+		}
+
+		// In terms of y = c0 + c1 x
+		double s2 = 0;
+		c1 = m_dxdy / m_dx2;
+		c0 = m_y - m_x * c1;
+
+		// Compute chi^2 = \sum (y_i - (a + b * x_i))^2
+		for (int i = 0; i < n; i++) {
+			double dx = x[i * xstride] - m_x;
+			double dy = y[i * ystride] - m_y;
+			double d = dy - c1 * dx;
+			sumsq += d * d;
+		}
+
+		s2 = sumsq / (n - 2.0); /* chisq per degree of freedom */
+
+		cov00 = s2 * (1.0 / n) * (1 + m_x * m_x / m_dx2);
+		cov11 = s2 * 1.0 / (n * m_dx2);
+		cov01 = s2 * (-m_x) / (n * m_dx2);
+
+	}
+	
+	public double c0(){
+		return c0;
+	}
+	
+	public double c1(){
+		return c1;
+	}
+	
+	public double covariance00(){
+		return cov00;
+	}
+	
+	public double covariance01(){
+		return cov01;
+	}
+
+	public double covariance11(){
+		return cov11;
+	}
+	
+	public double residual(){
+		return sumsq;
+	}
+}
---
+++ src/couk/nucmedone/common/gsl/LeastSquaresFitTest.java
@@ -0,0 +1,85 @@
+/**
+ * @author Neil
+ *
+ * Copyright (C) 2013 Neil
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or (at
+ * your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ */
+package couk.nucmedone.common.gsl;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Inspired by gsl-1.16/fit/test.c from the GNU Scientific Library project. <br />
+ * <br />
+ * Tests are based on the NIST Statistical Reference Datasets. See
+ * {@link www.nist.gov/itl/div898/strd/index.html} for more information
+ * 
+ * @author Neil
+ * 
+ */
+public class LeastSquaresFitTest {
+
+	private int norris_n = 36;
+
+	private double norris_x[] = { 0.2, 337.4, 118.2, 884.6, 10.1, 226.5, 666.3,
+			996.3, 448.6, 777.0, 558.2, 0.4, 0.6, 775.5, 666.9, 338.0, 447.5,
+			11.6, 556.0, 228.1, 995.8, 887.6, 120.2, 0.3, 0.3, 556.8, 339.1,
+			887.2, 999.0, 779.0, 11.1, 118.3, 229.2, 669.1, 448.9, 0.5 };
+
+	private double norris_y[] = { 0.1, 338.8, 118.1, 888.0, 9.2, 228.1, 668.5,
+			998.5, 449.1, 778.9, 559.2, 0.3, 0.1, 778.1, 668.8, 339.3, 448.9,
+			10.8, 557.7, 228.3, 998.0, 888.8, 119.6, 0.3, 0.6, 557.6, 339.3,
+			888.0, 998.5, 778.9, 10.2, 117.6, 228.9, 668.4, 449.2, 0.2 };
+
+	@Test
+	public void test() {
+
+		double[] x = new double[1000];
+		double[] y = new double[1000];
+		int xstride = 2;
+		int ystride = 5;
+
+		for (int i = 0; i < norris_n; i++) {
+			x[i * xstride] = norris_x[i];
+			y[i * ystride] = norris_y[i];
+		}
+
+		LeastSquaresFit lsf = new LeastSquaresFit(x, y, norris_n, xstride,
+				ystride);
+
+		double expected_c0 = -0.262323073774029;
+		Assert.assertEquals(expected_c0, lsf.c0(), 1e-10);
+
+		double expected_c1 = 1.00211681802045;
+		Assert.assertEquals(expected_c1, lsf.c1(), 1e-10);
+
+		double expected_cov00 = Math.pow(0.232818234301152, 2.0);
+		Assert.assertEquals(expected_cov00, lsf.covariance00(), 1e-10);
+
+		double expected_cov01 = -7.74327536339570e-05; /* computed from octave */
+		Assert.assertEquals(expected_cov01, lsf.covariance01(), 1e-10);
+
+		double expected_cov11 = Math.pow(0.429796848199937E-03, 2.0);
+		Assert.assertEquals(expected_cov11, lsf.covariance11(), 1e-10);
+
+		double expected_sumsq = 26.6173985294224;
+		Assert.assertEquals(expected_sumsq, lsf.residual(), 1e-10);
+
+	}
+
+}
