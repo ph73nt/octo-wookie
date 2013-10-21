c6b33b95e4898c1d96fe872b5696013ad73124ba
ph73nt <njt@fishlegs.co.uk>
2013-10-21 22:45:35 +0100

    Refactor decay, isotopes and nuclides classes into common package

---
+++ src/couk/nucmedone/common/nuclides/Isotopes.java
@@ -0,0 +1,9 @@
+package couk.nucmedone.common.nuclides;
+
+
+public class Isotopes {
+
+	public static final Nuclides cr51 = new Nuclides("cr51", "Chromium-51", 27.702511*24*60*60);
+	public static final Nuclides tc99m = new Nuclides("tc99m", "Technetium-99m", 6.0058*60*60);
+		
+}
---
+++ src/couk/nucmedone/common/nuclides/Nuclides.java
@@ -0,0 +1,36 @@
+package couk.nucmedone.common.nuclides;
+
+class Nuclides {
+	
+	private double halfLife;
+	private String name;
+	private String code;
+	
+	public Nuclides(String code, String name, double halfLife){
+		this.code = code;
+		this.name = name;
+		this.halfLife = halfLife;
+	}
+	
+	/**
+	 * Half-life of the nuclide in seconds.
+	 * 
+	 * @return The half-life (s)
+	 */
+	public double halfLife(){
+		return halfLife;
+	}
+	
+	public String name(){
+		return name;
+	}
+	
+	public String code(){
+		return code;
+	}
+	
+	public String toString() {
+		return name;
+	}
+
+}
